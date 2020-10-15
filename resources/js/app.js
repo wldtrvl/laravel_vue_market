fd40721e7ba4417ca4fce19939d0e9feaf2d6878
Ilya Marchenko <ilya@code-care.pro>
2020-10-15 04:13:37 +0300

    before add admin for backup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue vue. It will recursively scan this directory for the Vue
+ * vue and automatically register them with their "basename".
+ *
+ * Eg. ./vue/App.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('app', require('./vue/App.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding vue to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/vue/Components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello i`m card</h1>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+
+</style>
